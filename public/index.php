bcf0aeea75153d6e80d47c96410d35d17c46cb59
drydenmaker <drydenmaker@f860b17c-4e36-11de-9ee4-913ee803a954>
2010-02-02 16:58:34 +0000

    renamed theme dir

--- public/index.php
+++ public/index.php
@@ -1,55 +1,31 @@
 <?php
-//error_reporting(E_ALL|E_STRICT);
 session_start();
-//ob_start("ob_gzhandler");
+// enable output compression for all requests that can handle it
+ob_start("ob_gzhandler");
 
-require_once 'includes/X/Loader.php';
+require_once '../includes/X/Loader.php';
 
 X_Loader::registerAutoload();
-X_Loader::addIncludePath('includes/');
+// Crossley framework is not in the path
+X_Loader::addIncludePath('../includes/');
 
-// authentication hack
-if (array_key_exists('auth_token', $_SESSION) && $_SESSION['auth_token'] == md5(session_id().'dryden'))
-{
-     // pass   
-}
-else
-{
-    if (array_key_exists('q1947', $_POST) &&
-        $_POST['q1947'] == '.u5%-*' &&
-	    $_POST['p1947'] == 'admina')
-        {
-            $_SESSION['auth_token'] = md5(session_id().'dryden');
-        }
-        else 
-        {      
-       	    print "
-       	    	<form method='POST' action=''>
-       	    		Username: <input type='text' name='p1947'><br>
-       	    		Password: <input type='text' name='q1947'><br>
-       	    		<input type='SUBMIT' value='Login'>
-       	    	</form>
-       	    ";
-       	    exit(0);
-        }
-}
+X_Broker::setPath('../controllers/');
 
+// set up authentication
+require_once '../includes/authorities.php';
 
-if (function_exists('__autoload'))
-{
-    spl_autoload_register('__autoload');
-}
+// this will be used by the view engine for including styles
+X_Broker::registerValue('theme_dir', 'themes/default/');
 
-X_Broker::setPath('controllers/');
-X_Broker::registerValue('theme_dir', 'theme/default/');
-
+// get the registered instance of the broker (broker is not a singelton)
 $oBroker = X_Broker::getInstance();
+// allow for flat HTML file content
 $oBroker->setHtmlFileContent();
-
+	
+	// allow debuging
     $oBroker->setDebug();
+    // allow development generation
     $oBroker->setGenerate();
 
 // output the response
 print $oBroker->respond();
-
-//print X_Debug::out(X_Loader::$aTrace, ini_get('include_path'));
